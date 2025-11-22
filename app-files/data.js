58b31d4a95f63733127dea0f3ebd1d6841e7b406
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-11-22 03:04:03 -0500

    Commit

---
+++ app-files/data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-the-baptism-of-the-lord",
+      "name": "The Baptism of the Lord",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.000024339300006559483,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8577748219473857,
+          "pitch": 0.06846836247838262,
+          "rotation": 0,
+          "target": "1-the-wedding-feast-at-canna"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8471779434880631,
+          "pitch": -0.29544173465222023,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">The Baptism of Jesus&nbsp;</span>",
+          "text": "Then Jesus came from Galilee to the Jordan to be baptized by John. But John tried to deter him, saying, “I need to be baptized by you, and do you come to me?” 15 Jesus replied, “Let it be so now; it is proper for us to do this to fulfill all righteousness.” Then John consented. 16 As soon as Jesus was baptized, he went up out of the water. At that moment heaven was opened, and he saw the Spirit of God descending like a dove and alighting on him. 17 And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.” (Matthew 3:13–17)"
+        }
+      ]
+    },
+    {
+      "id": "1-the-wedding-feast-at-canna",
+      "name": "The Wedding Feast at Canna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.959611766112314,
+          "pitch": -0.030145689083878935,
+          "rotation": 0,
+          "target": "2-the-proclamation-of-the-kingdom"
+        },
+        {
+          "yaw": -1.7856730614706713,
+          "pitch": 0.06662837485324502,
+          "rotation": 0,
+          "target": "0-the-baptism-of-the-lord"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9495972579586827,
+          "pitch": -0.37702697702522414,
+          "title": "Jesus Changes Water Into Wine",
+          "text": "On the third day a wedding took place at Cana in Galilee. Jesus’ mother was there, and Jesus and his disciples had also been invited to the wedding. When the wine was gone, Jesus’ mother said to him, “They have no more wine.” “Woman, why do you involve me?” Jesus replied. “My hour has not yet come.” His mother said to the servants, “Do whatever he tells you.” Nearby stood six stone water jars, the kind used by the Jews for ceremonial washing, each holding from twenty to thirty gallons. Jesus said to the servants, “Fill the jars with water”; so they filled them to the brim. Then he told them, “Now draw some out and take it to the master of the banquet.” They did so, and the master of the banquet tasted the water that had been turned into wine. He did not realize where it had come from, though the servants who had drawn the water knew. Then he called the bridegroom aside and said, “Everyone brings out the choice wine first and then the cheaper wine after the guests have had too much to drink; but you have saved the best till now.” What Jesus did here in Cana of Galilee was the first of the signs through which he revealed his glory; and his disciples believed in him. (John 2:1-11)"
+        }
+      ]
+    },
+    {
+      "id": "2-the-proclamation-of-the-kingdom",
+      "name": "The Proclamation of The Kingdom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.451469258001941,
+          "pitch": 0.11188962560742155,
+          "rotation": 0,
+          "target": "1-the-wedding-feast-at-canna"
+        },
+        {
+          "yaw": -0.5754484819804588,
+          "pitch": -0.2037510964289062,
+          "rotation": 0,
+          "target": "0-the-baptism-of-the-lord"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5707284127477514,
+          "pitch": -0.40661578736549586,
+          "title": "Jesus Announces the Good News",
+          "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!” (Mark 1:14-15)"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
