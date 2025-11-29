d942fd75b286b1b66b84becb202235ef9f0df872
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-11-29 01:19:18 -0500

    update

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1-the-baptism-of-the-lord",
-      "name": "1. The Baptism of the Lord",
+      "id": "0-1-the-baptism-of-jesus",
+      "name": "1. The Baptism of Jesus",
       "levels": [
         {
           "tileSize": 256,
@@ -37,7 +37,7 @@ var APP_DATA = {
           "yaw": 0.8422053311738864,
           "pitch": 0.12158214276726298,
           "rotation": 0,
-          "target": "1-2-the-wedding-feast-at-canna"
+          "target": "1-2-the-wedding-feast-at-cana"
         }
       ],
       "infoHotspots": [
@@ -50,8 +50,8 @@ var APP_DATA = {
       ]
     },
     {
-      "id": "1-2-the-wedding-feast-at-canna",
-      "name": "2. The Wedding Feast at Canna",
+      "id": "1-2-the-wedding-feast-at-cana",
+      "name": "2. The Wedding Feast at Cana",
       "levels": [
         {
           "tileSize": 256,
@@ -92,7 +92,7 @@ var APP_DATA = {
           "yaw": -1.7917727884551393,
           "pitch": 0.12207156442657308,
           "rotation": 0,
-          "target": "0-1-the-baptism-of-the-lord"
+          "target": "0-1-the-baptism-of-jesus"
         }
       ],
       "infoHotspots": [
@@ -106,7 +106,7 @@ var APP_DATA = {
     },
     {
       "id": "2-3-the-proclamation-of-the-kingdom",
-      "name": "3. The Proclamation of The Kingdom",
+      "name": "3. The Proclamation of the Kingdom",
       "levels": [
         {
           "tileSize": 256,
@@ -138,30 +138,30 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.6308345798623289,
-          "pitch": -0.20245593392466077,
+          "yaw": -0.6309505617184872,
+          "pitch": -0.20506944323394194,
           "rotation": 0,
-          "target": "3-4-the-transfiguration-of-the-lord"
+          "target": "3-4-the-transfiguration"
         },
         {
-          "yaw": -2.4600318501156533,
-          "pitch": 0.07273442021745424,
+          "yaw": -2.4404910285766324,
+          "pitch": 0.11482987049295801,
           "rotation": 0,
-          "target": "1-2-the-wedding-feast-at-canna"
+          "target": "1-2-the-wedding-feast-at-cana"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.6659753935255743,
-          "pitch": -0.44997678993675194,
-          "title": "Jesus Announces the Good News",
+          "yaw": -0.666199223858321,
+          "pitch": -0.44900812976269044,
+          "title": "Jesus Announces The Good News",
           "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!” (Mark 1:14-15)"
         }
       ]
     },
     {
-      "id": "3-4-the-transfiguration-of-the-lord",
-      "name": "4. The Transfiguration of the Lord",
+      "id": "3-4-the-transfiguration",
+      "name": "4. The Transfiguration",
       "levels": [
         {
           "tileSize": 256,
@@ -192,6 +192,12 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
+        {
+          "yaw": 1.4671745023202245,
+          "pitch": 0.3824115641948662,
+          "rotation": 0,
+          "target": "4-5-the-institution-of-the-eucharist"
+        },
         {
           "yaw": -1.9109602901078429,
           "pitch": 0.06904835434230172,
@@ -204,7 +210,7 @@ var APP_DATA = {
           "yaw": 1.3629813253231013,
           "pitch": -0.11128284557314139,
           "title": "The Transfiguration",
-          "text": "he After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus. Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.” While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!” When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus.(Matthew 17:1-8)"
+          "text": "After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus. Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.” While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!” When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus. (Matthew 17:1-8)"
         }
       ]
     },
@@ -242,16 +248,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.9210662753430459,
-          "pitch": 0.06269100417794604,
+          "yaw": 0.9489095140136286,
+          "pitch": 0.008636074724844178,
           "rotation": 0,
-          "target": "3-4-the-transfiguration-of-the-lord"
+          "target": "3-4-the-transfiguration"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.5815730752985466,
-          "pitch": -0.3803506334926574,
+          "yaw": -0.7572829938780874,
+          "pitch": -0.4286953782456937,
           "title": "The Last Supper",
           "text": "While they were eating, Jesus took bread, and when he had given thanks, he broke it and gave it to his disciples, saying, “Take and eat; this is my body.” Then he took a cup, and when he had given thanks, he gave it to them, saying, “Drink from it, all of you. This is my blood of the covenant, which is poured out for many for the forgiveness of sins. (Matthew 26:26-28)"
         }
