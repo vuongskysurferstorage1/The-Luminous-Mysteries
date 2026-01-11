f7d973548dc4ca4375075a8f0702fbeb1cf5490c
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-11 02:18:08 -0500

    update

--- data.js
+++ data.js
@@ -34,24 +34,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.8422053311738864,
-          "pitch": 0.12158214276726298,
+          "yaw": 0.6768206640590719,
+          "pitch": 0.07746241482746186,
           "rotation": 0,
-          "target": "1-2-the-wedding-feast-at-cana"
+          "target": "1-2-the-wedding-feast-at-canna"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.8020021653021736,
-          "pitch": -0.21072106047657968,
+          "yaw": 0.6121699831532954,
+          "pitch": -0.2015709932610541,
           "title": "The Baptism of Jesus",
-          "text": "Then Jesus came from Galilee to the Jordan to be baptized by John. But John tried to deter him, saying, “I need to be baptized by you, and do you come to me?” Jesus replied, “Let it be so now; it is proper for us to do this to fulfill all righteousness.” Then John consented. As soon as Jesus was baptized, he went up out of the water. At that moment heaven was opened, and he saw the Spirit of God descending like a dove and alighting on him. And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.” (Matthew 3:13–17)"
+          "text": "Then Jesus came from Galilee to the Jordan to be baptized by John. But John tried to deter him, saying, “I need to be baptized by you, and do you come to me?”<div><br></div><div>Jesus replied, “Let it be so now; it is proper for us to do this to fulfill all righteousness.” Then John consented.</div><div><br></div><div>As soon as Jesus was baptized, he went up out of the water. At that moment heaven was opened, and he saw the Spirit of God descending like a dove and alighting on him. And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.”&nbsp;</div><div>(Matthew 3:13–17)</div>"
         }
       ]
     },
     {
-      "id": "1-2-the-wedding-feast-at-cana",
-      "name": "2. The Wedding Feast at Cana",
+      "id": "1-2-the-wedding-feast-at-canna",
+      "name": "2. The Wedding Feast at Canna",
       "levels": [
         {
           "tileSize": 256,
@@ -83,24 +83,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.957002453451576,
-          "pitch": 0.0048372278083768094,
+          "yaw": -0.20833050640655593,
+          "pitch": 0.01306316270420993,
           "rotation": 0,
           "target": "2-3-the-proclamation-of-the-kingdom"
         },
         {
-          "yaw": -1.7917727884551393,
-          "pitch": 0.12207156442657308,
+          "yaw": 1.3344809112445155,
+          "pitch": 0.10207835445671876,
           "rotation": 0,
           "target": "0-1-the-baptism-of-jesus"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.913130451884072,
-          "pitch": -0.2841497440968084,
+          "yaw": -0.2714829871675377,
+          "pitch": -0.26458510748860675,
           "title": "Jesus Changes Water Into Wine",
-          "text": "On the third day a wedding took place at Cana in Galilee. Jesus’ mother was there, and Jesus and his disciples had also been invited to the wedding. When the wine was gone, Jesus’ mother said to him, “They have no more wine.” “Woman, why do you involve me?” Jesus replied. “My hour has not yet come.” His mother said to the servants, “Do whatever he tells you.” Nearby stood six stone water jars, the kind used by the Jews for ceremonial washing, each holding from twenty to thirty gallons. Jesus said to the servants, “Fill the jars with water”; so they filled them to the brim. Then he told them, “Now draw some out and take it to the master of the banquet.” They did so, and the master of the banquet tasted the water that had been turned into wine. He did not realize where it had come from, though the servants who had drawn the water knew. Then he called the bridegroom aside and said, “Everyone brings out the choice wine first and then the cheaper wine after the guests have had too much to drink; but you have saved the best till now.” What Jesus did here in Cana of Galilee was the first of the signs through which he revealed his glory; and his disciples believed in him. (John 2:1-11)"
+          "text": "On the third day a wedding took place at Cana in Galilee. Jesus’ mother was there, and Jesus and his disciples had also been invited to the wedding. When the wine was gone, Jesus’ mother said to him, “They have no more wine.”<div><br></div><div>“Woman, why do you involve me?” Jesus replied. “My hour has not yet come.”</div><div><br></div><div>His mother said to the servants, “Do whatever he tells you.”</div><div><br></div><div>Nearby stood six stone water jars, the kind used by the Jews for ceremonial washing, each holding from twenty to thirty gallons.</div><div><br></div><div>Jesus said to the servants, “Fill the jars with water”; so they filled them to the brim.</div><div><br></div><div>Then he told them, “Now draw some out and take it to the master of the banquet.”<br></div><div><br></div><div>They did so, and the master of the banquet tasted the water that had been turned into wine. He did not realize where it had come from, though the servants who had drawn the water knew. Then he called the bridegroom aside and said, “Everyone brings out the choice wine first and then the cheaper wine after the guests have had too much to drink; but you have saved the best till now.”</div><div><br></div><div>What Jesus did here in Cana of Galilee was the first of the signs through which he revealed his glory; and his disciples believed in him.&nbsp;</div><div>(John 2:1-11)</div>"
         }
       ]
     },
@@ -138,24 +138,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.6309505617184872,
-          "pitch": -0.20506944323394194,
+          "yaw": 0.0421809875354473,
+          "pitch": -0.18435066272886402,
           "rotation": 0,
           "target": "3-4-the-transfiguration"
         },
         {
-          "yaw": -2.4404910285766324,
-          "pitch": 0.11482987049295801,
+          "yaw": -1.7177207594540604,
+          "pitch": 0.10515851495350681,
           "rotation": 0,
-          "target": "1-2-the-wedding-feast-at-cana"
+          "target": "1-2-the-wedding-feast-at-canna"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.666199223858321,
-          "pitch": -0.44900812976269044,
-          "title": "Jesus Announces The Good News",
-          "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!” (Mark 1:14-15)"
+          "yaw": -0.02827787623966671,
+          "pitch": -0.40272657443732207,
+          "title": "Jesus Announces the Good News",
+          "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!”&nbsp;<div>(Mark 1:14-15)</div>"
         }
       ]
     },
@@ -193,24 +193,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.4671745023202245,
-          "pitch": 0.3824115641948662,
-          "rotation": 0,
-          "target": "4-5-the-institution-of-the-eucharist"
-        },
-        {
-          "yaw": -1.9109602901078429,
-          "pitch": 0.06904835434230172,
+          "yaw": -1.7389854078994187,
+          "pitch": 0.04684428056571832,
           "rotation": 0,
           "target": "2-3-the-proclamation-of-the-kingdom"
+        },
+        {
+          "yaw": 1.750631465029147,
+          "pitch": 0.38559572674525455,
+          "rotation": 0,
+          "target": "4-5-the-institution-of-the-eucharist"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 1.3629813253231013,
-          "pitch": -0.11128284557314139,
+          "yaw": 1.6870822170237725,
+          "pitch": -0.1582484720333568,
           "title": "The Transfiguration",
-          "text": "After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus. Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.” While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!” When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus. (Matthew 17:1-8)"
+          "text": "After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus.<div><br></div><div>Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.”</div><div><br></div><div>While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!”</div><div><br></div><div>When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus. (Matthew 17:1-8)</div>"
         }
       ]
     },
@@ -248,18 +248,18 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.9489095140136286,
-          "pitch": 0.008636074724844178,
+          "yaw": 1.8030610600058088,
+          "pitch": 0.017110158319894708,
           "rotation": 0,
           "target": "3-4-the-transfiguration"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.781439655926162,
-          "pitch": -0.49173326528207184,
+          "yaw": 0.05781637618038005,
+          "pitch": -0.4788133969099242,
           "title": "The Last Supper",
-          "text": "While they were eating, Jesus took bread, and when he had given thanks, he broke it and gave it to his disciples, saying, “Take and eat; this is my body.” Then he took a cup, and when he had given thanks, he gave it to them, saying, “Drink from it, all of you. This is my blood of the covenant, which is poured out for many for the forgiveness of sins. (Matthew 26:26-28)"
+          "text": "While they were eating, Jesus took bread, and when he had given thanks, he broke it and gave it to his disciples, saying, “Take and eat; this is my body.”<div><br></div><div>Then he took a cup, and when he had given thanks, he gave it to them, saying, “Drink from it, all of you. This is my blood of the covenant, which is poured out for many for the forgiveness of sins. (Matthew 26:26-28)</div>"
         }
       ]
     }
